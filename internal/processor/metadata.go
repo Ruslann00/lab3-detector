ff71b911a1726c06f2089d8d5a20ecdf8ea315e5
Ruslan <stepanovigor3322@gmail.com>
2026-05-13 01:08:21 +0300

    feat: add processed images logging

--- internal/processor/metadata.go
+++ internal/processor/metadata.go
@@ -2,16 +2,27 @@ package processor
 
 import (
 	"fmt"
+	"log"
 	"regexp"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-var LeakCache = make(map[string][]byte)
+const maxCacheSize = 500
 
-var leakMu sync.Mutex
+var (
+	leakCacheMu sync.Mutex
+	LeakCache   = make(map[string][]byte, maxCacheSize)
+
+	imageRegexp = regexp.MustCompile(`^image_data_\d+_timestamp_\d+$`)
+
+	processedCount atomic.Uint64
+)
 
 func RunWorkerPool(count int) {
+	go logProcessedCount()
+
 	for i := 0; i < count; i++ {
 		go func(id int) {
 			for {
@@ -25,15 +36,48 @@ func RunWorkerPool(count int) {
 }
 
 func processImage(workerID int) {
-	data := fmt.Sprintf("image_data_%d_timestamp_%d", workerID, time.Now().UnixNano())
+	data := buildImageData(workerID)
 
-	matched, _ := regexp.MatchString(`^image_data_\d+_timestamp_\d+$`, data)
-
-	if matched {
-		key := fmt.Sprintf("key_%d", time.Now().UnixNano())
-
-		leakMu.Lock()
-		LeakCache[key] = make([]byte, 1024*10)
-		leakMu.Unlock()
+	if !imageRegexp.MatchString(data) {
+		return
 	}
+
+	saveToCache()
+	processedCount.Add(1)
+}
+
+func buildImageData(workerID int) string {
+	return fmt.Sprintf("image_data_%d_timestamp_%d", workerID, time.Now().UnixNano())
+}
+
+func saveToCache() {
+	leakCacheMu.Lock()
+	defer leakCacheMu.Unlock()
+
+	if len(LeakCache) >= maxCacheSize {
+		for key := range LeakCache {
+			delete(LeakCache, key)
+			break
+		}
+	}
+
+	key := fmt.Sprintf("key_%d", time.Now().UnixNano())
+	LeakCache[key] = make([]byte, 1024*10)
+}
+
+func logProcessedCount() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		log.Printf("processed images count: %d", GetProcessedCount())
+	}
+}
+
+func GetProcessedCount() uint64 {
+	return processedCount.Load()
+}
+
+func ResetProcessedCount() {
+	processedCount.Store(0)
 }
